e78ecae5f76848e91f8c322d30619fa22992bcc7
kenjiru <kenjiru.ro@gmail.com>
2023-01-29 22:22:05 +0100

    Add react-konva

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Stage, Layer, Rect, Circle } from 'react-konva';
 
 const App = () => (
-    <div>nothing</div>
+  <Stage width={window.innerWidth} height={window.innerHeight}>
+    <Layer>
+      <Rect width={50} height={50} fill="red" />
+      <Circle x={200} y={200} stroke="black" radius={50} />
+    </Layer>
+  </Stage>
 )
 
 export default App;
