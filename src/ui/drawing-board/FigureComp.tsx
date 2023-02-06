714ee30edd7fdd7abbcfb60cc1b074beb648a0b8
kenjiru <kenjiru.ro@gmail.com>
2023-02-06 22:42:07 +0100

    Make the selected figure draggable

--- src/ui/drawing-board/FigureComp.tsx
+++ src/ui/drawing-board/FigureComp.tsx
@@ -1,6 +1,7 @@
 import type { FC } from 'react';
 import { Rect } from 'react-konva';
 import { Figure } from '../../model/useEditorContent';
+import { useEditorSelection } from '../../model/useEditorSelection';
 
 interface Props {
     figure: Figure;
@@ -10,4 +11,19 @@ const DEFAULT_STROKE = 'red';
 
 export const FigureComp: FC<Props> = ({
     figure: { id, x, y, width, height },
-}) => <Rect id={id} x={x} y={y} width={width} height={height} stroke={DEFAULT_STROKE} />
+}) => {
+    const { selectedFigureId } = useEditorSelection();
+    const isDraggable = (id: string) => id === selectedFigureId;
+
+    return (
+        <Rect
+            id={id}
+            x={x}
+            y={y}
+            width={width}
+            height={height}
+            stroke={DEFAULT_STROKE}
+            draggable={isDraggable(id)}
+        />
+    );
+}
